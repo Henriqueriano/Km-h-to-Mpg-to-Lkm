62cf1432ad58ff5b6d77bde08218ef563b939c64
Daniel Henrique <113483288+Henriqueriano@users.noreply.github.com>
2023-10-04 15:08:14 -0300

    Add files via upload

---
+++ Combustivel.cs
@@ -0,0 +1,19 @@
+﻿namespace ExercicioUm
+{
+    public class Combustivel
+    {
+        public float consumo { get; set; }
+
+        public float ConverterMPG()
+        {
+            float varConsumo = consumo * 2.3521458357139f;
+            return varConsumo;
+        }
+
+        public float ConverterLkm()
+        {
+            float varConsumo = 100 / consumo;
+            return varConsumo;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace ExercicioUm;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Combustivel carroGenerico = new Combustivel();
+        Console.WriteLine("Informe o Consumo em Km/L:");
+        carroGenerico.consumo = float.Parse(Console.ReadLine()!);
+        Console.WriteLine($"\nConsumo Mpg: {carroGenerico.ConverterMPG()}\nConsumo L/100Km: {carroGenerico.ConverterLkm()}");
+    }
+}
